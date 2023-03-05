fdd169185211c27ee5b4f9fb966575125f5e5a59
김철우 <kimchul8747@daum.net>
2023-03-05 16:28:28 +0900

    ♻️ ::  코드 수정

--- React/google/src/component/footer/index.js
+++ React/google/src/component/footer/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 const FooterArea = styled.div`
-  width: 100%;
   height: 102px;
   background: #f2f2f2;
-
-  font-family: Apple SD Gothic Neo, arial, sans-serif;
 `;
 
 const FooterTop = styled.div`
@@ -19,19 +16,17 @@ const FooterBottom = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 0 20px;
+  font-size: 14px;
 `;
 
 const FooterBottomLeft = styled.div`
   padding: 15px 30px;
-  font-size: 14px;
   color: #70757a;
 `;
 
 const FooterBottomRight = styled.div`
   display: flex;
-  flex-wrap: wrap;
   padding: 15px 30px;
-  font-size: 14px;
   color: #70757a;
 `;
 
--- React/google/src/component/header/index.js
+++ React/google/src/component/header/index.js
@@ -8,7 +8,6 @@ const HeaderArea = styled.div`
   padding: 6px;
   height: 60px;
   font-size: 14px;
-  font-family: Apple SD Gothic Neo, arial, sans-serif;
 `;
 
 const HeaderLeft = styled.div`
@@ -17,7 +16,6 @@ const HeaderLeft = styled.div`
 
 const HeaderLeftFirst = styled.div`
   padding: 5px;
-  margin: 0 5px;
   margin-left: 15px;
 `;
 
@@ -28,7 +26,6 @@ const HeaderLeftSecond = styled.div`
 
 const HeaderRight = styled.div`
   display: flex;
-  float: right;
   padding-right: 15px;
 `;
 
@@ -45,21 +42,19 @@ export const Svg = styled.svg`
 
 const Header = () => {
   return (
-    <div>
-      <HeaderArea>
-        <HeaderLeft>
-          <HeaderLeftFirst>Google 정보</HeaderLeftFirst>
-          <HeaderLeftSecond>스토어</HeaderLeftSecond>
-        </HeaderLeft>
-        <HeaderRight>
-          <HeaderLeftSecond>Gmail</HeaderLeftSecond>
-          <HeaderLeftSecond>이미지</HeaderLeftSecond>
-          <Svg focusable="false" viewBox="0 0 24 24 ">
-            <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
-          </Svg>
-        </HeaderRight>
-      </HeaderArea>
-    </div>
+    <HeaderArea>
+      <HeaderLeft>
+        <HeaderLeftFirst>Google 정보</HeaderLeftFirst>
+        <HeaderLeftSecond>스토어</HeaderLeftSecond>
+      </HeaderLeft>
+      <HeaderRight>
+        <HeaderLeftSecond>Gmail</HeaderLeftSecond>
+        <HeaderLeftSecond>이미지</HeaderLeftSecond>
+        <Svg focusable="false" viewBox="0 0 24 24 ">
+          <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+        </Svg>
+      </HeaderRight>
+    </HeaderArea>
   );
 };
 
