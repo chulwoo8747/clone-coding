6d8ef0ba043c5f7277fce6a6a3ced3ff8132358f
김철우 <kimchul8747@daum.net>
2023-03-14 17:17:50 +0900

    ♻️ :: Heder style 수정

--- React/google/src/component/header/index.js
+++ React/google/src/component/header/index.js
@@ -25,14 +25,37 @@ const HeaderLeftSecond = styled.div`
 
 const HeaderRight = styled.div`
   display: flex;
+  align-items: center;
   padding-right: 15px;
+  justify-content: space-between;
 `;
 
 export const Svg = styled.svg`
   width: 24px;
   height: 24px;
   vertical-align: middle;
-  padding: 0 8px;
+`;
+
+const HeaderRightFirst = styled.div`
+  display: flex;
+  align-items: center;
+  padding-right: 15px; ;
+`;
+
+const HeaderRightSecond = styled.div`
+  padding: 4px;
+`;
+
+const Gmail = styled.div`
+  padding-left: 15px;
+`;
+
+const Picture = styled.div`
+  padding-left: 15px;
+`;
+
+const GoogleApp = styled.div`
+  padding: 8px;
 `;
 
 const Header = () => {
@@ -51,17 +74,23 @@ const Header = () => {
         </HeaderLeftSecond>
       </HeaderLeft>
       <HeaderRight>
-        <HeaderLeftSecond>
-          <a href="https://mail.google.com/mail/?authuser=0&ogbl">Gmail</a>
-        </HeaderLeftSecond>
-        <HeaderLeftSecond>
-          <a href="https://www.google.co.kr/imghp?hl=ko&authuser=0&ogbl">
-            이미지
-          </a>
-        </HeaderLeftSecond>
-        <Svg focusable="false" viewBox="0 0 24 24 ">
-          <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
-        </Svg>
+        <HeaderRightFirst>
+          <Gmail>
+            <a href="https://mail.google.com/mail/?authuser=0&ogbl">Gmail</a>
+          </Gmail>
+          <Picture>
+            <a href="https://www.google.co.kr/imghp?hl=ko&authuser=0&ogbl">
+              이미지
+            </a>
+          </Picture>
+        </HeaderRightFirst>
+        <HeaderRightSecond>
+          <GoogleApp>
+            <Svg focusable="false" viewBox="0 0 24 24">
+              <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+            </Svg>
+          </GoogleApp>
+        </HeaderRightSecond>
       </HeaderRight>
     </HeaderArea>
   );
